ecbd3939b4ecad56e093c9f8df5a8967737d27a5
Grace <gchen20@stuy.edu>
2020-10-27 22:57:16 -0400

    Tested get methods and set methods, tested withdrawal method

--- TesterBank.java
+++ TesterBank.java
@@ -1,4 +1,12 @@
 public class TesterBank {
   public static void main(String[]args) {
+    BankAccount a1 = new BankAccount(100, 12345, "password");
+    a1.setPassword("newpass");
+    System.out.println(a1.getBalance());
+    System.out.println(a1.getAccount());
+
+    System.out.println("Withdrawal of $50:");
+    System.out.println(a1.withdraw(50));
+    System.out.println(a1.getBalance());
   }
 }
